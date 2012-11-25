c058a5db6e894b08a21df3fa8811b64fc43b2e5c
IMBL-3B-CONTROL <antonmx@gmail.com>
2012-11-25 14:35:54 +1100

    modified:   scripts/preScan.inregular_z_step.sh

--- scripts/preScan.inregular_z_step.sh
+++ scripts/preScan.inregular_z_step.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # MUST BE FLOAT POINT, that is MUST HAVE DOT
 ZDS="
 0.0
@@ -9,7 +8,6 @@ ZDS="
 3.3	
 "
 
-echo $CURRENTZ  $SERIALMOTORPV
 zcur=0;
 foundz=""
 for tryz in $ZDS ; do 
@@ -21,8 +19,8 @@ done
 
 if echo $foundz | grep -q "^[0-9]*[.][0-9]*$" ; then
   echo "GOTO " $foundz
-  caput $SERIALMOTORPV $foundz
-  cawait ${SERIALMOTORPV}.DMOV 1
+  caput $MOTORTOMOVE $foundz
+  cawait ${MOTORTOMOVE}.DMOV 1
   exit 0
 else
   exit 1
