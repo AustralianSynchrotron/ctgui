c804d59f69c64e826fbd5ab630f5215c9d401a1d
Your Name <you@example.com>
2020-04-27 16:34:44 +1000

    new file:   libexec/pss.ct.sh

---
+++ libexec/pss.ct.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+PSS_POSITION_PV="SR99ID01DALSA:PPS:RotationPosition_RBV"
+counter=0
+
+camonitor -p 99 -t sI $PSS_POSITION_PV | sed -u -e "s/  \+/ /g"  -e "s +  g" |
+while read pv timel val ; do
+  if (( $(echo "$timel > 1" |bc -l) )) ; then
+    counter=0
+  else
+    if (( $counter > 5 )) ; then
+        caput "${DETECTOR_PV}:CAM:TriggerMode" 2
+        sleep 0.2s
+        kill -9 $(ps --ppid $$ | grep camonitor | cut -d' ' -f 1)
+        exit 0
+    fi
+    (( counter++ ))
+  fi
+done
+
